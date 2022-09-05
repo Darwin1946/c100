28c43aa5217c7ae4ff5e6f3f6c3e6fe1d64a5978
Darwin1946 <darwin931369866@gmail.com>
2022-09-05 23:30:11 +0800

    change

--- case15.c
+++ case15.c
@@ -1,18 +1,18 @@
 // 题目：利用条件运算符的嵌套来完成此题：学习成绩>=90分的同学用A表示，60-89分之间的用B表示，60分以下的用C表示。
 
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     int score;
     printf("get score:\n");
-    scanf("%d",&score);
-    if (score>=90)
+    scanf("%d", &score);
+    if (score >= 90)
     {
         printf("A\n");
-        
     }
-    else{
-        if (score>60)
+    else
+    {
+        if (score > 60)
         {
             printf("B\n");
         }
@@ -20,7 +20,5 @@ int main()
         {
             printf("C\n");
         }
-        
     }
-    
 }
