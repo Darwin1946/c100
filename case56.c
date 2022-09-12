3b58f6ad698c56b2e6b18defc91282df996e4889
Darwin1946 <darwin931369866@gmail.com>
2022-09-12 22:39:21 +0800

    缺少graphics库，绘图没法做

--- case56.c
+++ case56.c
@@ -1,20 +1,20 @@
 // 题目：画图，学用circle画圆形。
 //VC6.0中是不能运行的，要在Turbo2.0/3.0中  2
-#include <graphics.h>
-int main()   
-{  
-    int driver,mode,i;   
-    float j=1,k=1;   
-    driver=VGA;  
-    mode=VGAHI;   
-    initgraph(&driver,&mode,"");   
-    setbkcolor(YELLOW);   
-    for(i=0;i<=25;i++)   
-    {   
-        setcolor(8);   
-        circle(310,250,k);   
-        k=k+j;   
-    j=j+0.3;   
-    }   
-    return 0;  
-}
+// #include <graphics.h>
+// int main()   
+// {  
+//     int driver,mode,i;   
+//     float j=1,k=1;   
+//     driver=VGA;  
+//     mode=VGAHI;   
+//     initgraph(&driver,&mode,"");   
+//     setbkcolor(YELLOW);   
+//     for(i=0;i<=25;i++)   
+//     {   
+//         setcolor(8);   
+//         circle(310,250,k);   
+//         k=k+j;   
+//     j=j+0.3;   
+//     }   
+//     return 0;  
+// }
